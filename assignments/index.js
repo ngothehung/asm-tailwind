6272b5576be58a20bb7973348add662283a2a767
ngo the hung <ngothehung2110@gmail.com>
2022-06-09 10:54:17 +0700

    update

---
+++ assignments/index.js
@@ -0,0 +1,8 @@
+function handleClick() {
+  const menu = document.getElementById("main-menu")
+  if (menu.style.display === "block"){
+    menu.style.display = "none";
+  }else{
+    menu.style.display = "block";
+  }
+}
